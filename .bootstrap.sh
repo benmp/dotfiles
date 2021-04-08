a61d4e2346eefe86dc162c93230492c8098d28fc
runbmp <9081154+runbmp@users.noreply.github.com>
2021-04-07 20:56:12 -0500

    Update .bootstrap.sh

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 
-printf '\033[1;34m%s\033[0m\n' "upgrading all currently installed arch packages"
-sudo pacman -Syu
+statusprint() {
+  printf "\n"
+  printf "\033[${2:-1;34}%s\033[0m\n" "**********"
+  printf "\033[${2:-1;34}%s\033[0m\n" "*"
+  printf "\033[${2:-1;34}%s\033[0m\n" "*  $1"
+  printf "\033[${2:-1;34}%s\033[0m\n" "*"
+  printf "\033[${2:-1;34}%s\033[0m\n" "**********"
+  printf "\n"
+}
 
-printf '\033[1;34m%s\033[0m\n' "installing git for dotfiles grab"
+statusprint "upgrading all currently installed arch packages"
+sudo pacman -Syu
+statusprint "installing git for dotfiles grab"
 sudo pacman -S --needed git
 
 if [ ! -d "$HOME/.dotfiles" ]; then
-  printf '\033[1;34m%s\033[0m\n' "cloning dotfiles"
+  statusprint "cloning dotfiles"
   cd "$HOME"
   printf '%s\n' ".cfg" >> .gitignore
   git clone https://github.com/runbmp/dotfiles.git "$HOME/.dotfiles"
   /usr/bin/git --git-dir="$HOME/.dotfiles/.git" --work-tree="$HOME" config --local status.showUntrackedFiles no
 fi
 
-printf '\033[1;34m%s\033[0m\n' "checking out and updating dotfiles"
+statusprint "checking out and updating dotfiles"
 /usr/bin/git --git-dir="$HOME/.dotfiles/.git" --work-tree="$HOME" checkout master
 /usr/bin/git --git-dir="$HOME/.dotfiles/.git" --work-tree="$HOME" pull
 
+statusprint "source zshrc for environment variables"
 . "$HOME/.zshrc"
 
 arch_packages="\
@@ -53,25 +63,25 @@ termite-nocsd
 visual-studio-code-bin
 "
 
-printf '\033[1;34m%s\033[0m\n' "installing arch_packages: $arch_packages"
+statusprint "installing arch_packages: $arch_packages"
 sudo pacman -S --needed $(echo "$arch_packages")
 
 if [ -x "$(command -v yay)" ]; then
-  printf '\033[1;34m%s\033[0m\n' "installing yay aur helper"      
+  statusprint "installing yay aur helper"      
   git clone https://aur.archlinux.org/yay.git
   cd yay || exit
   makepkg -si
   cd ../
   rm -rf yay
 else
-  printf '\033[1;34m%s\033[0m\n' "yay aur helper already exists"
+  statusprint "yay aur helper already exists"
 fi
 
-printf '\033[1;34m%s\033[0m\n' "upgrading all currently installed aur packages"
+statusprint "upgrading all currently installed aur packages"
 yay -Syu --aur
 
 
-printf '\033[1;34m%s\033[0m\n' "installing aur_packages: $aur_packages"
+statusprint "installing aur_packages: $aur_packages"
 yay -S --needed $(echo "$aur_packages")
 
 udevmon_yaml="\
@@ -80,16 +90,16 @@ udevmon_yaml="\
     EVENTS:
       EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
 "
-printf '\033[1;34m%s\033[0m\n' "create caps2esc interception tools file"
+statusprint "create caps2esc interception tools file"
 printf '%s\n' "$udevmon_yaml" | sudo tee /etc/interception/udevmon.yaml
 
-printf '\033[1;34m%s\033[0m\n' "starting udevmon service"
+statusprint "starting udevmon service"
 sudo systemctl enable --now udevmon
 
 if ! [ -x "$(command -v n-update)" ]; then
-  printf '\033[1;34m%s\033[0m\n' "installing n to manage node versions"
+  statusprint "installing n to manage node versions"
   curl -L https://git.io/n-install | N_PREFIX=~/.n bash -s -- -y -
-else
-  printf '\033[1;34m%s\033[0m\n' "update n and use it to install node 10.24.0"
-  . "$HOME/.zshrc" && n-update -y && n 10.24.0
 fi
+
+statusprint "update n and use it to install node 10.24.0"
+. "$HOME/.zshrc" && n-update -y && n 10.24.0
