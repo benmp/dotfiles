28f0f0c025a07f00db30755450abb19232932d6d
runbmp <9081154+runbmp@users.noreply.github.com>
2021-04-12 07:39:03 -0500

    add scripts for screen share selection, screenshot copy, and screenshot save

---
+++ .local/bin/ss-wl.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+readonly SCREENSHOTDIR="$HOME/.cache/screenshots"
+
+if [[ ! -e "$SCREENSHOTDIR" ]]; then
+  mkdir -p "$SCREENSHOTDIR"
+fi
+
+readonly TIME="$(date +%Y-%m-%d-%H-%M-%S)"
+readonly IMGPATH="$SCREENSHOTDIR/img-$TIME.png"
+grim -g "$(slurp)" "$IMGPATH"
+share "$IMGPATH"
